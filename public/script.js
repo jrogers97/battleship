b50c435bf03eeef8253c9cc9bd11b81ca6f89c0d
Jake Rogers <jake@dataplusmath.com>
2020-10-12 01:03:48 -0400

    fix formatting issue

--- public/script.js
+++ public/script.js
@@ -5,23 +5,23 @@ $(document).ready(function () {
 	const infoOverlay = $(".overlay");
 	const yourGrid = $(".your-grid");
 	const theirGrid = $(".their-grid");
-    const statusBar = $(".status-bar");
-    const randomizeBtn = $(".randomize-btn");
-    const dragShip = $(".drag-ship");
-    const dragShipOutline = $(".drag-ship-outline");
-    
+	const statusBar = $(".status-bar");
+	const randomizeBtn = $(".randomize-btn");
+	const dragShip = $(".drag-ship");
+	const dragShipOutline = $(".drag-ship-outline");
+
 	let ships;
 	let isYourTurn;
 	// cells that have a ship
 	let shipIdxs;
 	// cells that have been hit
-    let hitIdxs = [];
-    // cells with/adjacent to a ship
-    let takenIdxs = [];
-    
-    fillGrids();
-    drawRandomShips();
-    randomizeBtn.on("click", drawRandomShips);
+	let hitIdxs = [];
+	// cells with/adjacent to a ship
+	let takenIdxs = [];
+
+	fillGrids();
+	drawRandomShips();
+	randomizeBtn.on("click", drawRandomShips);
 
 	let gameId = window.location.pathname.split("/")[1];
 	if (gameId) {
@@ -34,11 +34,11 @@ $(document).ready(function () {
 		history.pushState({}, null, gameId);
 	}
 
-    function drawRandomShips() {
-        ships = randomizeShips();
-        calculateShipInfo(ships);
-        drawShips();
-    }
+	function drawRandomShips() {
+		ships = randomizeShips();
+		calculateShipInfo(ships);
+		drawShips();
+	}
 
 	function fillGrids() {
 		$(".grid-wrapper").each(function () {
@@ -61,17 +61,17 @@ $(document).ready(function () {
 	}
 
 	function drawShips() {
-        // reset all cells first
-        $(".your-grid .cell").removeClass("with-ship");
+		// reset all cells first
+		$(".your-grid .cell").removeClass("with-ship");
 
-        // add ship class to relevant cells
-		$(".your-grid .cell").each(function(idx) {
+		// add ship class to relevant cells
+		$(".your-grid .cell").each(function (idx) {
 			if (shipIdxs.indexOf(idx) > -1) {
 				$(this).addClass("with-ship");
-            }
-        });
-        
-        bindMouseDown(true);
+			}
+		});
+
+		bindMouseDown(true);
 	}
 
 	function markFiredCell(markYourGrid, cell, wasHit, wasSunk, hitShip) {
@@ -106,7 +106,10 @@ $(document).ready(function () {
 	}
 
 	function handlePlayerJoined() {
-		const startBtn = $(`<button class="btn">Play</button>`).on("click", handlePlayerReady);
+		const startBtn = $(`<button class="btn">Play</button>`).on(
+			"click",
+			handlePlayerReady
+		);
 		showInfoOverlay(`Ready to start?`);
 		infoOverlay.append(startBtn);
 	}
@@ -116,10 +119,10 @@ $(document).ready(function () {
 			if (!gameStarted) {
 				showInfoOverlay("Waiting for your opponent...");
 			}
-        });
-        
-        randomizeBtn.css("visibility", "hidden");
-        bindMouseDown(false);
+		});
+
+		randomizeBtn.css("visibility", "hidden");
+		bindMouseDown(false);
 	}
 
 	function handlePlayerTurn(playerId) {
@@ -194,8 +197,8 @@ $(document).ready(function () {
 
 		if (gameOver) {
 			statusBar.addClass(wasYourFire ? "won" : "lost");
-            statusBar.html(wasYourFire ? "You won!" : "You lost :(");
-            theirGrid.addClass("disabled");
+			statusBar.html(wasYourFire ? "You won!" : "You lost :(");
+			theirGrid.addClass("disabled");
 		}
 	}
 
@@ -240,12 +243,13 @@ $(document).ready(function () {
 
 	function randomizeShips() {
 		const shipLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
-        const ships = [];
-        takenIdxs = [];
+		const ships = [];
+		takenIdxs = [];
 
 		shipLengths.forEach((len) => {
 			const availSpots = makeAvailableShipSpots(len);
-			const ship = availSpots[Math.floor(Math.random() * availSpots.length)];
+			const ship =
+				availSpots[Math.floor(Math.random() * availSpots.length)];
 			const offLimits = calculateSurroundingShipCells(ship);
 
 			ships.push(ship);
@@ -273,28 +277,29 @@ $(document).ready(function () {
 			}
 			if (cell / 10 + length <= 10) {
 				vertSpot = [...Array(length).keys()].map((i) => cell + i * 10);
-            }
-            
-            const filteredSpots = [horizSpot, vertSpot].filter(spot => {
-                return spot && !spot.some(i => takenIdxs.indexOf(i) > -1 || i < 0 || i > 99);
-            });
+			}
+
+			const filteredSpots = [horizSpot, vertSpot].filter((spot) => {
+				return (
+					spot &&
+					!spot.some(
+						(i) => takenIdxs.indexOf(i) > -1 || i < 0 || i > 99
+					)
+				);
+			});
 
 			spots.push(...filteredSpots);
-        });
-        return spots;
-    }
+		});
+		return spots;
+	}
 
-    function calculateAllOffLimitCells(ships) {
+	function calculateAllOffLimitCells(ships) {
 		return ships.reduce((acc, ship) => {
-			return [
-                ...acc, 
-                ...ship,
-                ...calculateSurroundingShipCells(ship)
-            ];
+			return [...acc, ...ship, ...calculateSurroundingShipCells(ship)];
 		}, []);
 	}
 
-    function calculateShipOffLimitCells(cell, wasSunk, hitShip) {
+	function calculateShipOffLimitCells(cell, wasSunk, hitShip) {
 		if (wasSunk && hitShip) {
 			return calculateSurroundingShipCells(hitShip);
 		}
@@ -333,126 +338,161 @@ $(document).ready(function () {
 		return [...new Set(offLimits)].filter(
 			(c) => c >= 0 && c <= 99 && ship.indexOf(c) < 0
 		);
-    }
+	}
 
-    function calculateShipInfo(ships) {
-        shipIdxs = ships.reduce((acc, ship) => [...acc, ...ship], []);
-        takenIdxs = calculateAllOffLimitCells(ships);
-    }
-    
-    // ***************** DRAG & DROP ********************
+	function calculateShipInfo(ships) {
+		shipIdxs = ships.reduce((acc, ship) => [...acc, ...ship], []);
+		takenIdxs = calculateAllOffLimitCells(ships);
+	}
 
-    let heldShip;
-    let newShipSpot;
-    let mouseOffset = [0, 0];
-    
-    $(document).on("mousedown", e => e.preventDefault());
-    $(document).on("mouseup", () => {
-        if (heldShip) {
-            dragShip.hide();
-            dragShipOutline.hide();
-            if (newShipSpot) {
-                // draw new ship
-                newShipSpot.forEach(cell => yourGrid.find(`.cell[data-cell=${cell}]`).addClass("with-ship"));
-                // replace old ship with new spot
-                const oldShipIdx = ships.findIndex(ship => ship.indexOf(heldShip[0]) > -1);
-                ships = [
-                    ...ships.slice(0, oldShipIdx),
-                    newShipSpot,
-                    ...ships.slice(oldShipIdx + 1)
-                ];
-                // re-calculate ship/taken idxs
-                calculateShipInfo(ships);
-                // re-bind mouse down for future ship moves
-                bindMouseDown(true);
-            } else {
-                // re-draw original dragged ship
-                heldShip.forEach(cell => yourGrid.find(`.cell[data-cell=${cell}]`).addClass("with-ship"));
-            }
-            heldShip = null;
-            newShipSpot = null;
-        }
-    });
+	// ***************** DRAG & DROP ********************
 
-    function bindMouseDown(enable) {
-        const yourShips = yourGrid.find(".with-ship");
-        yourShips.off("mousedown");
-        if (enable) {
-            yourGrid.find(".with-ship").on("mousedown", function(e) {
-                heldShip = ships.find(s => s.indexOf(Number($(this).attr("data-cell"))) > -1);
-                if (heldShip) { 
-                    // hide actual ship that's being dragged
-                    heldShip.forEach(cell => yourGrid.find(`.cell[data-cell="${cell}"]`).removeClass("with-ship"));
-                    
-                    const firstShipCell = yourGrid.find(`.cell[data-cell="${heldShip[0]}"]`);
-                    const isHorizontal = heldShip.length === 1 || heldShip[1] - heldShip[0] === 1;
-                    mouseOffset = [
-                        firstShipCell[0].offsetLeft - e.clientX,
-                        firstShipCell[0].offsetTop - e.clientY,
-                    ];
-                    // place "fake" ship where original ship was
-                    dragShip.css({
-                        display: "block",
-                        width: isHorizontal ? heldShip.length * 40 : 40,
-                        height: !isHorizontal ? heldShip.length * 40 : 40,
-                        top: firstShipCell[0].offsetTop,
-                        left: firstShipCell[0].offsetLeft,
-                    });
-                }
-            });
-        } else {
-            yourShips.addClass("game-started");
-        }
-    }
+	let heldShip;
+	let newShipSpot;
+	let mouseOffset = [0, 0];
 
-    $(document).on("mousemove", function(e) {
-        e.preventDefault();
-        // follow mouse on drag
-        if (heldShip) {
-            const mouseLeft = e.clientX + mouseOffset[0];
-            const mouseTop = e.clientY + mouseOffset[1];
-            dragShip.css({
-                left: mouseLeft,
+	$(document).on("mousedown", (e) => e.preventDefault());
+	$(document).on("mouseup", () => {
+		if (heldShip) {
+			dragShip.hide();
+			dragShipOutline.hide();
+			if (newShipSpot) {
+				// draw new ship
+				newShipSpot.forEach((cell) =>
+					yourGrid
+						.find(`.cell[data-cell=${cell}]`)
+						.addClass("with-ship")
+				);
+				// replace old ship with new spot
+				const oldShipIdx = ships.findIndex(
+					(ship) => ship.indexOf(heldShip[0]) > -1
+				);
+				ships = [
+					...ships.slice(0, oldShipIdx),
+					newShipSpot,
+					...ships.slice(oldShipIdx + 1),
+				];
+				// re-calculate ship/taken idxs
+				calculateShipInfo(ships);
+				// re-bind mouse down for future ship moves
+				bindMouseDown(true);
+			} else {
+				// re-draw original dragged ship
+				heldShip.forEach((cell) =>
+					yourGrid
+						.find(`.cell[data-cell=${cell}]`)
+						.addClass("with-ship")
+				);
+			}
+			heldShip = null;
+			newShipSpot = null;
+		}
+	});
+
+	function bindMouseDown(enable) {
+		const yourShips = yourGrid.find(".with-ship");
+		yourShips.off("mousedown");
+		if (enable) {
+			yourGrid.find(".with-ship").on("mousedown", function (e) {
+				heldShip = ships.find(
+					(s) => s.indexOf(Number($(this).attr("data-cell"))) > -1
+				);
+				if (heldShip) {
+					// hide actual ship that's being dragged
+					heldShip.forEach((cell) =>
+						yourGrid
+							.find(`.cell[data-cell="${cell}"]`)
+							.removeClass("with-ship")
+					);
+
+					const firstShipCell = yourGrid.find(
+						`.cell[data-cell="${heldShip[0]}"]`
+					);
+					const isHorizontal =
+						heldShip.length === 1 ||
+						heldShip[1] - heldShip[0] === 1;
+					mouseOffset = [
+						firstShipCell[0].offsetLeft - e.clientX,
+						firstShipCell[0].offsetTop - e.clientY,
+					];
+					// place "fake" ship where original ship was
+					dragShip.css({
+						display: "block",
+						width: isHorizontal ? heldShip.length * 40 : 40,
+						height: !isHorizontal ? heldShip.length * 40 : 40,
+						top: firstShipCell[0].offsetTop,
+						left: firstShipCell[0].offsetLeft,
+					});
+				}
+			});
+		} else {
+			yourShips.addClass("game-started");
+		}
+	}
+
+	$(document).on("mousemove", function (e) {
+		e.preventDefault();
+		// follow mouse on drag
+		if (heldShip) {
+			const mouseLeft = e.clientX + mouseOffset[0];
+			const mouseTop = e.clientY + mouseOffset[1];
+			dragShip.css({
+				left: mouseLeft,
 				top: mouseTop,
 			});
-            
-            // check where dragged ship would be dropped, outline if valid
-            const startCell = Math.round(mouseTop / 40) * 10 + Math.round(mouseLeft / 40);
-            const isHorizontal = heldShip.length === 1 || heldShip[1] - heldShip[0] === 1;
 
-            // make sure ship starting at start cell fits in the grid
-            if (!isValidStartCell(startCell, heldShip.length, isHorizontal)) {
-                return;
-            }
+			// check where dragged ship would be dropped, outline if valid
+			const startCell =
+				Math.round(mouseTop / 40) * 10 + Math.round(mouseLeft / 40);
+			const isHorizontal =
+				heldShip.length === 1 || heldShip[1] - heldShip[0] === 1;
 
-            // make new ship for potential placement
-            const newShipSpotPotential = [...Array(heldShip.length).keys()]
-                .map(i => startCell + (i * (isHorizontal ? 1 : 10)));
-                
-            // ignore the currently dragged ship when determining valid new spots
-            const shipsIgnoringHeldShip = ships.filter(ship => ship.indexOf(heldShip[0]) < 0);
-            const takenIdxsIgnoringHeldShip = calculateAllOffLimitCells(shipsIgnoringHeldShip);
+			// make sure ship starting at start cell fits in the grid
+			if (!isValidStartCell(startCell, heldShip.length, isHorizontal)) {
+				return;
+			}
 
-            const validNewShipSpot = !newShipSpotPotential.some(cell => takenIdxsIgnoringHeldShip.indexOf(cell) > -1);
-            if (validNewShipSpot) {
-                // show that ship can be dropped here
-                dragShipOutline.css({
+			// make new ship for potential placement
+			const newShipSpotPotential = [...Array(heldShip.length).keys()].map(
+				(i) => startCell + i * (isHorizontal ? 1 : 10)
+			);
+
+			// ignore the currently dragged ship when determining valid new spots
+			const shipsIgnoringHeldShip = ships.filter(
+				(ship) => ship.indexOf(heldShip[0]) < 0
+			);
+			const takenIdxsIgnoringHeldShip = calculateAllOffLimitCells(
+				shipsIgnoringHeldShip
+			);
+
+			const validNewShipSpot = !newShipSpotPotential.some(
+				(cell) => takenIdxsIgnoringHeldShip.indexOf(cell) > -1
+			);
+			if (validNewShipSpot) {
+				// show that ship can be dropped here
+				dragShipOutline.css({
 					display: "block",
 					width: isHorizontal ? newShipSpotPotential.length * 40 : 40,
-					height: !isHorizontal ? newShipSpotPotential.length * 40 : 40,
+					height: !isHorizontal
+						? newShipSpotPotential.length * 40
+						: 40,
 					top: Math.floor(newShipSpotPotential[0] / 10) * 40,
 					left: (newShipSpotPotential[0] % 10) * 40,
-                });
-                newShipSpot = newShipSpotPotential;
-            } else {
-                newShipSpot = null;
-                dragShipOutline.hide();
-            }
-        }
-    });
+				});
+				newShipSpot = newShipSpotPotential;
+			} else {
+				newShipSpot = null;
+				dragShipOutline.hide();
+			}
+		}
+	});
 
-    function isValidStartCell(cell, shipLength, isHorizontal) {
-        return cell >= 0 && cell < 100
-            && (isHorizontal ? cell % 10 : Math.floor(cell / 10)) + shipLength <= 10;
-    }
+	function isValidStartCell(cell, shipLength, isHorizontal) {
+		return (
+			cell >= 0 &&
+			cell < 100 &&
+			(isHorizontal ? cell % 10 : Math.floor(cell / 10)) + shipLength <=
+				10
+		);
+	}
 });
