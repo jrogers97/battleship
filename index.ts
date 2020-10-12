cb5a5774ccfd1da7e536ca586b8f7289cfb4dd55
Jake Rogers <jake@dataplusmath.com>
2020-10-12 19:42:47 -0400

    Heroku

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ const io: SocketIO.Server = socketio(server);
 app.use("dist/public", express.static(path.join(__dirname, "dist/public")));
 // send index.html for all requests
 app.get("/*", (req, res) => {
-	res.sendFile(__dirname + "/dist/public/index.html");
+	res.sendFile(__dirname + "/public/index.html");
 });
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
